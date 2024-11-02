d9d59b15ad3785cd55c2913ed30d18eaaa0417af
ImRaffy14 <raffysolis123@gmail.com>
2024-11-02 15:33:27 +0800

    added: animations on emotion page

--- src/pages/emotions.jsx
+++ src/pages/emotions.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import anger from '../assets/emotions/anger.png';
 import anxious from '../assets/emotions/anxious.png';
 import ennui from '../assets/emotions/ennui.png';
@@ -10,6 +11,35 @@ import ReactPlayer from 'react-player';
 
 
 function Emotions() {
+
+    const [isTypingDone, setIsTypingDone] = useState(false);
+
+    const handleTypingComplete = () => {
+      console.log("Typing complete!");
+      sessionStorage.setItem('isTypingDone', 'true');
+    };
+  
+    useEffect(() => {
+        
+        const typingDone = sessionStorage.getItem('isTypingDone');
+        if (typingDone === 'true') {
+            setIsTypingDone(true);
+            return
+        }
+        
+        const textLength = 580;
+        const typeSpeed = 60; 
+        const delay = textLength * typeSpeed;
+    
+        const timer = setTimeout(() => {
+            setIsTypingDone(true);
+            handleTypingComplete();
+        }, delay);
+    
+        return () => clearTimeout(timer);
+    }, []);
+
+    
   return (
     <>
       <ReactPlayer
@@ -26,7 +56,7 @@ function Emotions() {
           <div className='flex flex-col sm:flex-row items-center'>
             <img src={echo} alt='echo' className='h-40 sm:h-[250px] mt-4 sm:mt-11 animate-pulse' />
             
-            <div class="max-w-md bg-white text-gray-800 p-4 rounded-lg shadow-md ml-5">
+            <div class="max-w-md bg-white text-gray-800 p-4 rounded-lg shadow-md">
                 <Typewriter
                 words={[`Hey there, I’m Echo!
                 Welcome to PsychBeats, your personal space for emotional growth and balance. I’m here to guide you through the soothing world of AI-generated music. Whether you’re feeling stressed, need a focus boost, or just want to lift your mood, I’ve got the perfect track for you.
@@ -43,25 +73,50 @@ function Emotions() {
 
           </div>
 
-            <h1 className='text-white mt-10 mb-5 text-xl'><strong>What are you feeling today?</strong></h1>
+          {isTypingDone && (
+            <div className={`fade-in ${isTypingDone ? 'fade-in-active' : ''}`}>
+              <h1 className="text-white mt-10 mb-5 text-xl text-center">
+                <strong>What are you feeling today?</strong>
+              </h1>
 
-            <div className='flex flex-wrap justify-center items-center gap-4 '>
+              <div className="flex flex-wrap justify-center items-center gap-4 ">
                 <Link to="/anger">
-                <img src={anger} alt="anger" className='h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer' />
+                  <img
+                    src={anger}
+                    alt="anger"
+                    className="h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer"
+                  />
                 </Link>
 
                 <Link to="/anxious">
-                <img src={anxious} alt="anxious" className='h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer' />
+                  <img
+                    src={anxious}
+                    alt="anxious"
+                    className="h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer"
+                  />
                 </Link>
 
                 <Link to="/sadness">
-                <img src={sadness} alt="ennui" className='h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer' />
+                  <img
+                    src={sadness}
+                    alt="sadness"
+                    className="h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer"
+                  />
                 </Link>
 
                 <Link to="/ennui">
-                <img src={ennui} alt="sadness" className='h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer' />
+                  <img
+                    src={ennui}
+                    alt="ennui"
+                    className="h-40 sm:h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-105 cursor-pointer"
+                  />
                 </Link>
+              </div>
             </div>
+          )}
+
+
+
         </div>
       </div>
     </>
